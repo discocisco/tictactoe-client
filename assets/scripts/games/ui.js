cdc6ba14f30c4d70bb67921119f94396a5653034
discocisco <f.andresort@gmail.com>
2019-01-06 17:49:38 -0500

    Game update feature: Curl
    Add game-board to `index.html`
    Remove game-board creator in `ui.js`

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -5,19 +5,7 @@ const store = require('../store.js')
 const onCreateGameSuccess = (responseData) => {
   store.game = responseData.game
   $('#user-message').html(`Successfully created a new game! Game ID is ${store.game.id}`)
-  $('#game-board').html(`
-    <button id="box-0" type="button" class="btn btn-primary col-4 col-md-4 box" aria-disabled="false"></button>
-    <button id="box-1" type="button" class="btn btn-primary col-4 col-md-4 box" aria-disabled="false"></button>
-    <button id="box-2" type="button" class="btn btn-primary col-4 col-md-4 box" aria-disabled="false"></button>
-
-    <button id="box-3" type="button" class="btn btn-primary col-4 col-md-4 box" aria-disabled="false"></button>
-    <button id="box-4" type="button" class="btn btn-primary col-4 col-md-4 box" aria-disabled="false"></button>
-    <button id="box-5" type="button" class="btn btn-primary col-4 col-md-4 box" aria-disabled="false"></button>
-
-    <button id="box-6" type="button" class="btn btn-primary col-4 col-md-4 box" aria-disabled="false"></button>
-    <button id="box-7" type="button" class="btn btn-primary col-4 col-md-4 box" aria-disabled="false"></button>
-    <button id="box-8" type="button" class="btn btn-primary col-4 col-md-4 box" aria-disabled="false"></button>
-    `)
+  $('#game-board .row .btn').removeAttr('disabled')
 }
 
 const onIndexGamesSuccess = (responseData) => {
