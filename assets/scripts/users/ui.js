85f4c18b16dfba4f2a2899f948d7afff6c992e61
discocisco <f.andresort@gmail.com>
2019-01-07 17:46:30 -0500

    Remove console.log in `users`
    Clean console by removing several logs

--- assets/scripts/users/ui.js
+++ assets/scripts/users/ui.js
@@ -3,20 +3,16 @@
 const store = require('../store.js')
 
 const onSignUpSuccess = (responseData) => {
-  console.log(responseData)
   $('#user-message').html('Successfully signed up!')
 }
 
 const onSignInSuccess = (responseData) => {
-  console.log(responseData)
   store.user = responseData.user
   $('#user-message').html(`Successfully logged in as: ${store.user.email}`)
-  console.log('Store is:', store)
 }
 
 const onChangePasswordSuccess = () => {
   $('#user-message').html(`Successfully changed password for: ${store.user.email}`)
-  console.log('Store is:', store)
 }
 
 const onSignOutSuccess = () => {
